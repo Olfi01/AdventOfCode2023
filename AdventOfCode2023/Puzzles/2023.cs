bbd801919d8f9ebab350739105276f4b7743729d
Olfi01 <flom@meyer-alpers.de>
2023-12-04 11:51:50 +0100

    Day 4

--- AdventOfCode2023/Puzzles/2023.cs
+++ AdventOfCode2023/Puzzles/2023.cs
@@ -12,6 +12,10 @@ namespace AdventOfCode2023.Puzzles
 {
     public class _2023
     {
+        #region General constants
+        private static readonly char[] digits = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        #endregion
+
         #region Day 1
         private static string[] ReadInputDay1(string input)
         {
@@ -101,6 +105,7 @@ namespace AdventOfCode2023.Puzzles
 
         #endregion
 
+        #region Day 2
         public static List<CubeGame> ReadInputDay2(string input)
         {
             return input.Split("\n").Where(x => !string.IsNullOrEmpty(x)).Select(l => new CubeGame(l)).ToList();
@@ -178,5 +183,199 @@ namespace AdventOfCode2023.Puzzles
             }
             return (r, g, b);
         }
+
+        #endregion
+
+        #region Day 3
+        private static string[] ReadInputDay3(string input)
+        {
+            return input.Split("\n").Where(x => !string.IsNullOrEmpty(x)).ToArray();
+        }
+
+
+        [Puzzle(day: 3, part: 1)]
+        public static void Day3Part1(string input, Grid display, Label outputLabel)
+        {
+            string[] engineChart = ReadInputDay3(input);
+
+            int sum = 0;
+            TextBlock debug = new();
+            display.Children.Add(debug);
+
+            for (int row = 0; row < engineChart.Length; row++)
+            {
+                string line = engineChart[row];
+                int? numberStart = null;
+                for (int col = 0; col < line.Length; col++)
+                {
+                    if (numberStart.HasValue)
+                    {
+                        if (!digits.Contains(line[col]))
+                        {
+                            int number = int.Parse(line[numberStart.Value..col]);
+                            if (HasAdjacentSymbol(row, from: numberStart.Value, to: col, engineChart))
+                            {
+                                sum += number;
+                                debug.Inlines.Add(new Run(number + "\n") { Foreground = Brushes.Green });
+                            }
+                            else
+                            {
+                                debug.Inlines.Add(new Run(number + "\n") { Foreground = Brushes.Red });
+                            }
+                            numberStart = null;
+                        }
+                    }
+                    else
+                    {
+                        if (digits.Contains(line[col]))
+                        {
+                            numberStart = col;
+                        }
+                    }
+                }
+                if (numberStart.HasValue)
+                {
+                    int number = int.Parse(line[numberStart.Value..]);
+                    if (HasAdjacentSymbol(row, from: numberStart.Value, to: line.Length - 1, engineChart))
+                    {
+                        sum += number;
+                        debug.Inlines.Add(new Run(number + "\n") { Foreground = Brushes.Green });
+                    }
+                    else
+                    {
+                        debug.Inlines.Add(new Run(number + "\n") { Foreground = Brushes.Red });
+                    }
+                }
+            }
+
+            outputLabel.Content = sum;
+        }
+
+        private static bool HasAdjacentSymbol(int row, int from, int to, string[] engineChart)
+        {
+            if (from != 0) from--;
+            if (to != engineChart[0].Length - 1) to++;
+            if (engineChart[row][from..to].Any(IsSymbol)) return true;
+            if (row > 0 && engineChart[row - 1][from..to].Any(IsSymbol)) return true;
+            if (row < engineChart.Length - 1 && engineChart[row + 1][from..to].Any(IsSymbol)) return true;
+            return false;
+        }
+
+        private static bool IsSymbol(char c)
+        {
+            return c != '.' && !digits.Contains(c);
+        }
+
+
+        [Puzzle(day: 3, part: 2)]
+        public static void Day3Part2(string input, Grid display, Label outputLabel)
+        {
+            string[] engineChart = ReadInputDay3(input);
+
+            int sum = 0;
+            TextBlock debug = new();
+            display.Children.Add(debug);
+            List<(int number, int row, int startIndex, int endIndex)> numbers = new();
+
+            for (int row = 0; row < engineChart.Length; row++)
+            {
+                string line = engineChart[row];
+                int? numberStart = null;
+                for (int col = 0; col < line.Length; col++)
+                {
+                    if (numberStart.HasValue)
+                    {
+                        if (!digits.Contains(line[col]))
+                        {
+                            int number = int.Parse(line[numberStart.Value..col]);
+                            numbers.Add((number, row, numberStart.Value, col));
+                            numberStart = null;
+                        }
+                    }
+                    else
+                    {
+                        if (digits.Contains(line[col]))
+                        {
+                            numberStart = col;
+                        }
+                    }
+                }
+                if (numberStart.HasValue)
+                {
+                    int number = int.Parse(line[numberStart.Value..]);
+                    numbers.Add((number, row, numberStart.Value, line.Length - 1));
+                }
+            }
+
+            for (int row = 0; row < engineChart.Length; row++)
+            {
+                string line = engineChart[row];
+                for (int col = 0; col < line.Length; col++)
+                {
+                    if (IsSymbol(line[col]) && HasTwoAdjacentNumbers(row, col, numbers))
+                    {
+                        (int num1, int num2) = GetTwoAdjacentNumbers(row, col, numbers);
+                        sum += num1 * num2;
+                    }
+                }
+            }
+
+            outputLabel.Content = sum;
+        }
+
+        private static bool HasTwoAdjacentNumbers(int row, int col, List<(int number, int row, int startIndex, int endIndex)> numbers)
+        {
+            return numbers.Count(n => IsNumberAdjacentToGear(n, row, col)) == 2;
+        }
+
+        private static bool IsNumberAdjacentToGear((int number, int row, int startIndex, int endIndex) n, int row, int col)
+        {
+            return n.row >= row - 1 && n.row <= row + 1 && n.startIndex <= col + 1 && n.endIndex >= col;
+        }
+
+        private static (int num1, int num2) GetTwoAdjacentNumbers(int row, int col, List<(int number, int row, int startIndex, int endIndex)> numbers)
+        {
+            var results = numbers.Where(n => IsNumberAdjacentToGear(n, row, col));
+            return (results.ElementAt(0).number, results.ElementAt(1).number);
+        }
+        #endregion
+
+        #region Day 4
+        private static ScratchCard[] ReadInputDay4(string input)
+        {
+            return input.Split("\n").Where(x => !string.IsNullOrEmpty(x)).Select(s => new ScratchCard(s)).ToArray();
+        }
+
+
+        [Puzzle(day: 4, part: 1)]
+        public static void Day4Part1(string input, Grid display, Label outputLabel)
+        {
+            ScratchCard[] cards = ReadInputDay4(input);
+
+            int sum = cards.Sum(c => c.GetPoints());
+
+            outputLabel.Content = sum;
+        }
+
+
+        [Puzzle(day: 4, part: 2)]
+        public static void Day4Part2(string input, Grid display, Label outputLabel)
+        {
+            ScratchCard[] cards = ReadInputDay4(input);
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                var card = cards[i];
+                for (int j = 1; j <= card.MatchingNumbers; j++)
+                {
+                    cards[i + j].Copies += card.Copies;
+                }
+            }
+
+            int sum = cards.Sum(c => c.Copies);
+
+            outputLabel.Content = sum;
+        }
+        #endregion
     }
 }
---
+++ AdventOfCode2023/Puzzles/Classes_2023/ScratchCard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Puzzles.Classes_2023
+{
+    public record ScratchCard
+    {
+        public int Id { get; }
+        public int[] WinningNumbers { get; }
+        public int[] MyNumbers { get; }
+        public int MatchingNumbers { get => MyNumbers.Count(n => WinningNumbers.Contains(n)); }
+        public int Copies { get; set; } = 1;
+
+        public ScratchCard(string input)
+        {
+            string[] split = input.Split(':');
+            Id = int.Parse(split[0][5..]);
+            string[] numbers = split[1].Split('|');
+            WinningNumbers = numbers[0].Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).Select(int.Parse).ToArray();
+            MyNumbers = numbers[1].Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).Select(int.Parse).ToArray();
+        }
+
+        public int GetPoints()
+        {
+            if (MatchingNumbers == 0) return 0;
+            return (int)Math.Pow(2, MatchingNumbers - 1);
+        }
+    }
+}
